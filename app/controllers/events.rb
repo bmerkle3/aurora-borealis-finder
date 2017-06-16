fc7eda0aaa0a9ef88a3dcbb42f274654896caed0
Braidy Merkle <braidymerkle@gmail.com>
2017-06-15 21:37:06 -0500

    map space shows, but map errors

--- app/controllers/events.rb
+++ app/controllers/events.rb
@@ -4,15 +4,14 @@ end
 
 
 post '/events' do
-  uri = URI('https://api.auroras.live/v1/?type=all&lat=40.7813913&long=-73.976902&forecast=false&threeday=false')
-  http_string = Net::HTTP.get(uri)
-  parsed_response = JSON.parse(http_string)
-  event = parsed_response["probability"]["highest"]
-  puts "reg event: #{event}"
-  @event = Event.create(color: event["colour"], long: event["long"], lat: event["lat"], color: event["color"], value: event["value"])
-  puts "@event: #{@event}"
-
-  redirect "/events/#{@event.id}"
+  # uri = URI('https://api.auroras.live/v1/?type=all&lat=40.7813913&long=-73.976902&forecast=false&threeday=false')
+  # http_string = Net::HTTP.get(uri)
+  # parsed_response = JSON.parse(http_string)
+  # event = parsed_response["probability"]["highest"]
+  # puts "reg event: #{event}"
+  # @event = Event.create(color: event["colour"], long: event["long"], lat: event["lat"], color: event["color"], value: event["value"])
+  redirect "/events/5"
+  # redirect "/events/#{@event.id}"
 end
 
 
