82f9419f2a79afcccf0362340dd341023f1a26c9
Braidy Merkle <braidymerkle@gmail.com>
2017-06-16 13:40:43 -0500

    background picture fills whole page without white space

--- app/controllers/events.rb
+++ app/controllers/events.rb
@@ -4,14 +4,14 @@ end
 
 
 post '/events' do
-  # uri = URI('https://api.auroras.live/v1/?type=all&lat=40.7813913&long=-73.976902&forecast=false&threeday=false')
-  # http_string = Net::HTTP.get(uri)
-  # parsed_response = JSON.parse(http_string)
-  # event = parsed_response["probability"]["highest"]
-  # puts "reg event: #{event}"
-  # @event = Event.create(color: event["colour"], long: event["long"], lat: event["lat"], value: event["value"])
-  redirect "/events/2"
-  # redirect "/events/#{@event.id}"
+  uri = URI('https://api.auroras.live/v1/?type=all&lat=40.7813913&long=-73.976902&forecast=false&threeday=false')
+  http_string = Net::HTTP.get(uri)
+  parsed_response = JSON.parse(http_string)
+  event = parsed_response["probability"]["highest"]
+  puts "reg event: #{event}"
+  @event = Event.create(color: event["colour"], long: event["long"], lat: event["lat"], value: event["value"])
+  # redirect "/events/2"
+  redirect "/events/#{@event.id}"
 end
 
 
